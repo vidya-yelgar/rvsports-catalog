6dce926bbf07d1146c7e6bb87760ad1003a9befd
vidya-yelgar <vidyamagdum08@gmail.com>
2026-07-18 23:11:15 +0530

    Add search feature for card filtering
    Implement search functionality to filter cards based on user input.

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const search = document.getElementById("search");
 
+search.addEventListener("keyup", function(){
+
+let value = this.value.toLowerCase();
+
+let card = document.querySelector(".card");
+
+let title = card.querySelector("h2").innerText.toLowerCase();
+
+if(title.includes(value))
+{
+card.style.display="block";
+}
+else
+{
+card.style.display="none";
+}
+
+});
