6df1374aeacb9e3a9df753934af76af9180733fe
vidya-yelgar <vidyamagdum08@gmail.com>
2026-07-18 23:23:08 +0530

    Add product fetching and display functionality
    Fetch products from JSON and display in cards.

--- product.js
+++ product.js
@@ -1,1 +1,36 @@
+fetch("products.json")
+.then(response => response.json())
+.then(products => {
 
+const container =
+document.getElementById("products");
+
+products.forEach(product=>{
+
+container.innerHTML += `
+
+<div class="card">
+
+<img src="${product.image}">
+
+<h2>${product.name}</h2>
+
+<p>₹${product.price}</p>
+
+<a href="product.html?id=${product.id}">
+
+<button>
+
+View Details
+
+</button>
+
+</a>
+
+</div>
+
+`;
+
+});
+
+});
